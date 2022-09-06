87f69f3cf70a4e8538e47daf2d410585b3ff6a6f
tanaka <lemo.nade.engineer.room@gmail.com>
2022-09-07 02:41:41 +0900

    ログイン画面の入力コンポーネントの実装

---
+++ frontend/src/views/LoginView.vue
@@ -0,0 +1,26 @@
+<script lang="ts" setup>
+import ShortInput from "@/components/inputs/ShortInput.vue";
+import GoButton from "@/components/buttons/GoButton.vue";
+</script>
+
+<template>
+  <div class="login">
+    <ShortInput class="input-id" placeholder="ID" />
+    <ShortInput
+      class="input-password"
+      type="password"
+      placeholder="パスワード"
+    />
+    <GoButton />
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.login {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  height: 100vh;
+}
+</style>
