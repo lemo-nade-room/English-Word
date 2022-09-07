fe6b9e251f8814919822252690599ed8ee0cb8a5
tanaka <lemo.nade.engineer.room@gmail.com>
2022-09-07 12:58:09 +0900

    Plusボタンを実装

--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import TheWelcome from "../components/TheWelcome.vue";
+import PlusButton from "@/components/buttons/PlusButton.vue";
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+  <div class="home">
+    <PlusButton class="plus" />
+  </div>
 </template>
+
+<style lang="scss" scoped>
+.home {
+  width: 100vw;
+  height: 100vh;
+  .plus {
+    position: absolute;
+    right: 21px;
+    top: 27px;
+  }
+}
+</style>
