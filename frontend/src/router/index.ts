045ad7ae49afdc156af36f118127e18c118a7f20
tanaka <lemo.nade.engineer.room@gmail.com>
2022-09-07 15:38:57 +0900

    タイトルのデザインを実装

--- frontend/src/router/index.ts
+++ frontend/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
 import LoginView from "@/views/LoginView.vue";
 import CreateView from "@/views/CreateView.vue";
+import ModeView from "@/views/ModeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -28,6 +29,11 @@ const router = createRouter({
       path: "/create",
       name: "create",
       component: CreateView,
+    },
+    {
+      path: "/mode",
+      name: "mode",
+      component: ModeView,
     },
   ],
 });
---
+++ frontend/src/views/ModeView.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import Title from "@/components/header/Title.vue";
+</script>
+<template>
+  <div class="mode">
+    <Title text="Make Make Make Make Make Make Make Make" />
+  </div>
+</template>
+
+<style lang="scss " scoped></style>
